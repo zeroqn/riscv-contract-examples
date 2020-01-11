bcbe8c811c9ba64cce1efbce3c04848e8f1c17d7
zero.qn <zero.qn@outlook.com>
2020-01-11 08:47:27 +0800

    Refactor ballot contract
    Fix some bugs

--- c/ballot.c
+++ c/ballot.c
@@ -3,99 +3,108 @@
 
 #include "pvm.h"
 #include "pvm_extend.h"
-#include "helper.h"
 
-// Common data
-const uint8_t MAX_TOPIC_SIZE = 100;
-const uint64_t MAX_VOTER_SIZE = 1 << 30;
+// Common size
+#define ADDRESS_SIZE 20
+#define MAX_TOPIC_LEN 100
+#define MAX_VOTER_COUNT 1 << 30
+
+// Common bool
+#define TRUE 0
+#define FALSE 1
 
 // Common keys
-const char *OWNER_KEY = "owner_key";
-const char *TOPIC_KEY = "topic_key";
-const char *STATE_KEY = "state_key";
-const char *TOTAL_VOTER = "total_voter_key";
-const char *YEA_COUNT_KEY = "yea_count_key";
+#define OWNER_KEY "owner_key"
+#define TOPIC_KEY "topic_key"
+#define STATE_KEY "state_key"
+#define TOTAL_VOTER "total_voter"
+#define YEA_COUNT_KEY "yea_count_key"
+
+#define YEA_STR "1"
+#define NAY_STR "2"
 
 // Ballot state
-const uint8_t PREPARE = 0;
-const uint8_t STARTED = 1;
-const uint8_t END = 2;
-
-// Ballot Result
-const uint8_t APPROVED = 0;
-const uint8_t FAILED = 1;
-
-// Vote value
-const uint8_t NAY = 0;
-const uint8_t YEA = 1;
-const uint8_t NONE = 2;
-
-const char *NAY_STR = "0";
-const char *YEA_STR = "1";
+#define PREPARE 0
+#define STARTED 1
+#define END 2
 
 // Return
-const int SUCCESS = 0;
+#define SUCCESS 0
 
 // Errors
-const int ERR_NO_KEY = 1;
-const int ERR_METHOD_ARG_NUM = 2;
-const int ERR_TOPIC_NO_SET = 3;
-const int ERR_TOPIC_ALREADY_SET = 4;
-const int ERR_NO_PERMISSION = 5;
-const int ERR_TOPIC_EXCEE_MAX_SIZE = 6;
-const int ERR_INVALID_VOTE_VALUE = 7;
-const int ERR_MAX_VOTER_REACHED = 8;
-const int ERR_VOTE_ALREADY_STARTED_OR_END = 9;
-const int ERR_VOTE_NOT_STARTED_YET = 10;
-const int ERR_VOTE_NOT_END_YET = 10;
+#define ERR_METHOD_ARG_NUM 100
+#define ERR_OWNER_NO_SET 101
+#define ERR_OWNER_ALREADY_SET 102
+#define ERR_TOPIC_NO_SET 103
+#define ERR_TOPIC_ALREADY_SET 104
+#define ERR_NO_PERMISSION 105
+#define ERR_EXCEE_MAX_LEN 106
+#define ERR_INVALID_VOTE_VALUE 107
+#define ERR_MAX_VOTER_REACHED 108
+#define ERR_VOTE_ALREADY_STARTED_OR_END 108
+#define ERR_VOTE_NOT_STARTED_YET 109
+#define ERR_VOTE_NOT_END_YET 110
+#define ERR_ALREADY_VOTED 111
+#define ERR_INVALID_ADDRESS 112
 
-// APIs
-const char *SET_TOPIC = "set_topic";
-const char *AUTHORIZE_VOTE_RIGHT = "authorize_vote_right";
-const char *GET_OWNER = "get_owner";
-const char *GET_TOPIC = "get_topic";
-const char *IS_VOTER = "is_voter";
-const char *VOTE = "vote";
-const char *START_VOTE = "start_vote";
-const char *END_VOTE = "end_vote";
-const char *ANN_RESULT = "ann_result";
+// API names
+#define SET_OWNER "set_owner" // We don't have constructor yet
+#define GET_OWNER "get_owner"
+#define GET_TOPIC "get_topic"
+#define SET_TOPIC "set_topic"
+#define AUTHORIZE_VOTE_RIGHT "authorize_vote_right"
+#define IS_VOTER "is_voter"
+#define START_VOTE "start_vote"
+#define END_VOTE "end_vote"
+#define VOTE "vote"
+#define ANN_RESULT "ann_result"
 
-int is_owner(uint8_t* owner_addr) {
-    uint8_t caller[ADDRESS_SIZE];
-    pvm_caller(&caller[0]);
+// Constants
+// Use const uint8_t so that we can easily pvm_save and pvm_ret
 
-    return match_addr(&caller[0], &owner_addr[0]);
-}
+const uint8_t HAS_VOTER_RIGHT = 1;
+const uint8_t NO_RIGHT = 2;
 
-int has_vote_right(uint8_t* addr) {
-    if (FALSE == is_valid_addr(addr)) {
-        return ERR_INVALID_ADDRESS;
-    }
+// Vote value
+const uint8_t NONE = 0;
+const uint8_t YEA = 1;
+const uint8_t NAY = 2;
 
-    int ret;
-    uint8_t vote[1];
+// Result
+const uint8_t APPROVED = 1;
+const uint8_t FAILED = 2;
 
-    ret = pvm_load(&addr[0], ADDRESS_SIZE, &vote[0], 1, NULL);
-    if (ERR_NO_KEY == ret) {
-        return FALSE;
-    } else {
-        return TRUE;
-    }
-}
+
+// Types
+typedef struct addr_t { uint8_t addr[ADDRESS_SIZE]; } addr_t;
+typedef struct topic_t { char str[MAX_TOPIC_LEN]; } topic_t;
+
+// Voter
+// First u8: 1 means has voter right
+// Second u8: 1 means yea, 2 means nay
+typedef struct voter_t { uint8_t state[2]; } voter_t;
+
+
+// Contract helpers
+int is_empty(uint8_t* val, uint64_t max_size);
+int addr_mached(addr_t caller, addr_t expect);
+int is_owner(addr_t owner_addr);
+int has_vote_right(addr_t caller);
+int already_voted(addr_t caller);
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     return SUCCESS;
   }
 
-  uint8_t *owner_key = (uint8_t *)strstr(OWNER_KEY, OWNER_KEY);
-  uint8_t *topic_key = (uint8_t *)strstr(TOPIC_KEY, TOPIC_KEY);
-  uint8_t *state_key = (uint8_t *)strstr(STATE_KEY, STATE_KEY);
-  uint8_t *total_voter_key = (uint8_t *)strstr(TOTAL_VOTER, TOTAL_VOTER);
-  uint8_t *yea_count_key = (uint8_t *)strstr(YEA_COUNT_KEY, YEA_COUNT_KEY);
+  uint8_t *owner_key = (uint8_t *)OWNER_KEY;
+  uint8_t *topic_key = (uint8_t *)TOPIC_KEY;
+  uint8_t *state_key = (uint8_t *)STATE_KEY;
+  uint8_t *total_voter_key = (uint8_t *)TOTAL_VOTER;
+  uint8_t *yea_count_key = (uint8_t *)YEA_COUNT_KEY;
 
-  uint8_t owner[ADDRESS_SIZE];
-  uint8_t topic[MAX_TOPIC_SIZE];
+  addr_t owner;
+  topic_t topic;
   uint8_t state;
   uint64_t total_voter = 0;
   uint64_t yea_count = 0;
@@ -104,115 +113,129 @@ int main(int argc, char *argv[]) {
 
   // Load contract owner, ERR_NO_KEY means we are deploying contract,
   // set owner to contract deployer.
-  int has_owner =
-      pvm_load(&owner_key[0], ADDRESS_SIZE, &owner[0], ADDRESS_SIZE, NULL);
-
-  if (ERR_NO_KEY == has_owner) {
-    pvm_caller(&owner[0]);
-    pvm_save(&owner_key[0], strlen(OWNER_KEY), &owner[0], ADDRESS_SIZE);
-  }
+  pvm_load(owner_key, strlen(OWNER_KEY), owner.addr, ADDRESS_SIZE, NULL);
 
   // Load topic
-  int has_topic = pvm_load(&topic_key[0], strlen(TOPIC_KEY), &topic[0],
-                       MAX_TOPIC_SIZE, NULL);
+  pvm_load(topic_key, strlen(TOPIC_KEY), (uint8_t *)topic.str, MAX_TOPIC_LEN, NULL);
 
   // Load state value
-  uint8_t sv[1];
-  int has_state = pvm_load(&state_key[0], strlen(STATE_KEY), &sv[0], 1, NULL);
-
-  if (ERR_NO_KEY == has_state) {
-      sv[0] = PREPARE;
-      pvm_save(&state_key[0], strlen(STATE_KEY), &sv[0], 1);
-  }
+  uint8_t sv[1] = { 0 };
+  pvm_load(&state_key[0], strlen(STATE_KEY), &sv[0], 1, NULL);
   state = sv[0];
 
   // Load voter count
-  uint8_t vc[8];
-  int has_voter = pvm_load(&total_voter_key[0], strlen(TOTAL_VOTER), &vc[0], 8, NULL);
-
-  if (SUCCESS == has_voter) {
-      total_voter = *vc;
-  }
+  uint8_t vc[8] = { 0 };
+  pvm_load(&total_voter_key[0], strlen(TOTAL_VOTER), &vc[0], 8, NULL);
+  total_voter = *vc;
 
   // Load yea count
   uint8_t yc[8];
-  int has_yea = pvm_load(&yea_count_key[0], strlen(YEA_COUNT_KEY), &yc[0], 8, NULL);
-
-  if (SUCCESS == has_yea) {
-      yea_count = *yc;
-  }
-
+  pvm_load(&yea_count_key[0], strlen(YEA_COUNT_KEY), &yc[0], 8, NULL);
+  yea_count = *yc;
 
   // ### Contract APIs
 
+  if (strcmp(argv[1], SET_OWNER) == 0) {
+      if (argc != 2) {
+          return ERR_METHOD_ARG_NUM;
+      }
+
+      if (FALSE == is_empty(owner.addr, ADDRESS_SIZE)) {
+          return ERR_OWNER_ALREADY_SET;
+      }
+
+      addr_t caller;
+      pvm_caller(caller.addr);
+
+      pvm_save(owner_key, strlen(OWNER_KEY), caller.addr, ADDRESS_SIZE);
+      
+      return SUCCESS;
+  }
+
   // Return owner address
-  if (strcmp(argv[1], GET_OWNER) == 0) {
+  else if (strcmp(argv[1], GET_OWNER) == 0) {
     if (argc != 2) {
       return ERR_METHOD_ARG_NUM;
     }
 
-    pvm_ret(&owner[0], ADDRESS_SIZE);
+    pvm_ret(owner.addr, ADDRESS_SIZE);
     return SUCCESS;
   }
 
+  if (TRUE == is_empty(owner.addr, ADDRESS_SIZE)) {
+      pvm_debug("no owner");
+      return ERR_OWNER_NO_SET;
+  }
+
   // Return ballot topic
-  else if (strcmp(argv[1], GET_TOPIC)) {
+  if (strcmp(argv[1], GET_TOPIC) == 0) {
     if (argc != 2) {
       return ERR_METHOD_ARG_NUM;
     }
 
-    if (ERR_NO_KEY == has_topic) {
-      return ERR_TOPIC_NO_SET;
+    if (TRUE == is_empty((uint8_t *)topic.str, strlen(topic.str))) {
+        return ERR_TOPIC_NO_SET;
     }
 
-    pvm_ret(&topic[0], strlen((char *)&topic[0]));
+    pvm_ret((uint8_t *)topic.str, strlen(topic.str));
     return SUCCESS;
   }
 
   // Set ballot topic
-  else if (strcmp(argv[1], SET_TOPIC)) {
+  else if (strcmp(argv[1], SET_TOPIC) == 0) {
     if (argc != 3) {
       return ERR_METHOD_ARG_NUM;
     }
 
-    if (ERR_NO_KEY != has_topic) {
-      return ERR_TOPIC_ALREADY_SET;
+    if (FALSE == is_empty((uint8_t *)topic.str, strlen(topic.str))) {
+        return ERR_TOPIC_NO_SET;
     }
 
-    if (FALSE == is_owner(&owner[0])) {
+    addr_t caller;
+    pvm_caller(caller.addr);
+
+    if (FALSE == is_owner(caller)) {
         return ERR_NO_PERMISSION;
     }
 
-    uint8_t *topic = (uint8_t *)argv[2];
-    if (MAX_TOPIC_SIZE < ARRAY_LEN(&topic)) {
-        return ERR_TOPIC_EXCEE_MAX_SIZE;
+    char* topic = argv[2];
+    if (MAX_TOPIC_LEN < strlen(topic)) {
+        return ERR_EXCEE_MAX_LEN;
     }
-
-    pvm_save(&topic_key[0], strlen(TOPIC_KEY), &topic[0], ARRAY_LEN(&topic));
+    
+    pvm_save(topic_key, strlen(TOPIC_KEY), (uint8_t *)topic, strlen(topic));
     return SUCCESS;
   }
 
   // Authorize voter right to given address
-  else if (strcmp(argv[1], AUTHORIZE_VOTE_RIGHT)) {
+  else if (strcmp(argv[1], AUTHORIZE_VOTE_RIGHT) == 0) {
     if (argc != 3) {
         return ERR_METHOD_ARG_NUM;
     }
 
-    if (FALSE == is_owner(&owner[0])) {
+    addr_t caller;
+    pvm_caller(caller.addr);
+
+    if (FALSE == is_owner(caller)) {
         return ERR_NO_PERMISSION;
     }
 
-    uint8_t *auth_addr = (uint8_t *)argv[2];
-    if (FALSE == is_valid_addr(&auth_addr[0])) {
+    if (total_voter == MAX_VOTER_COUNT) {
+        return ERR_MAX_VOTER_REACHED;
+    }
+    
+    // Address is passed by hex encoded
+    if (40 != strlen(argv[2])) {
         return ERR_INVALID_ADDRESS;
     }
 
-    if (total_voter == MAX_VOTER_SIZE) {
-        return ERR_MAX_VOTER_REACHED;
-    }
+    addr_t user;
+    pvm_hex2bin(argv[2], (char *)user.addr);
+    
+    voter_t voter;
+    voter.state[0] = HAS_VOTER_RIGHT;
 
-    uint8_t vote[1] = {NONE};
-    pvm_save(&auth_addr[0], ADDRESS_SIZE, &vote[0], 1);
+    pvm_save(user.addr, ADDRESS_SIZE, voter.state, sizeof(voter.state));
 
     ++total_voter;
     pvm_save(&total_voter_key[0], strlen(TOTAL_VOTER), (uint8_t *)total_voter, 8);
@@ -221,59 +244,26 @@ int main(int argc, char *argv[]) {
   }
 
   // Check whether caller has voter right
-  else if (strcmp(argv[1], IS_VOTER)) {
+  else if (strcmp(argv[1], IS_VOTER) == 0) {
       if (argc != 2) {
           return ERR_METHOD_ARG_NUM;
       }
 
-      uint8_t caller[ADDRESS_SIZE];
-      pvm_caller(&caller[0]);
+      addr_t caller;
+      pvm_caller(caller.addr);
 
-      int is_voter = has_vote_right(&caller[0]);
-      if (ERR_NO_KEY == is_voter) {
-          pvm_ret((uint8_t *)&FALSE, 1);
+      int is_voter = has_vote_right(caller);
+      if (FALSE == is_voter) {
+          pvm_ret((uint8_t *)&NO_RIGHT, 1);
       } else {
-          pvm_ret((uint8_t *)&TRUE, 1);
+          pvm_ret((uint8_t *)&HAS_VOTER_RIGHT, 1);
       }
-
-      return SUCCESS;
-  }
-
-  // Vote yea or nay to ballot topic
-  else if (strcmp(argv[1], VOTE)) {
-      if (argc != 3) {
-          return ERR_METHOD_ARG_NUM;
-      }
-
-      if (STARTED != state) {
-          return ERR_VOTE_NOT_STARTED_YET;
-      }
-
-      uint8_t vote[1];
-      if (strcmp(argv[2], YEA_STR)) {
-          vote[0] = YEA;
-      }
-      else if (strcmp(argv[2], NAY_STR)) {
-          vote[0] = NAY;
-      }
-      else {
-          return ERR_INVALID_VOTE_VALUE;
-      }
-
-      uint8_t caller[ADDRESS_SIZE];
-      pvm_caller(&caller[0]);
-
-      if (FALSE == has_vote_right(&caller[0])) {
-          return ERR_NO_PERMISSION;
-      }
-
-      pvm_save(&caller[0], ADDRESS_SIZE, &vote[0], 1);
 
       return SUCCESS;
   }
 
   // Start vote phrase
-  else if (strcmp(argv[1], START_VOTE)) {
+  else if (strcmp(argv[1], START_VOTE) == 0) {
       if (argc != 2) {
           return ERR_METHOD_ARG_NUM;
       }
@@ -281,8 +271,11 @@ int main(int argc, char *argv[]) {
       if (PREPARE != state) {
           return ERR_VOTE_ALREADY_STARTED_OR_END;
       }
+      
+      addr_t caller;
+      pvm_caller(caller.addr);
 
-      if (FALSE == is_owner(&owner[0])) {
+      if (FALSE == is_owner(caller)) {
           return ERR_NO_PERMISSION;
       }
 
@@ -293,7 +286,7 @@ int main(int argc, char *argv[]) {
   }
 
   // End vote phrase
-  else if (strcmp(argv[1], END_VOTE)) {
+  else if (strcmp(argv[1], END_VOTE) == 0) {
       if (argc != 2) {
           return ERR_METHOD_ARG_NUM;
       }
@@ -302,7 +295,10 @@ int main(int argc, char *argv[]) {
           return ERR_VOTE_NOT_STARTED_YET;
       }
 
-      if (FALSE == is_owner(&owner[0])) {
+      addr_t caller;
+      pvm_caller(caller.addr);
+
+      if (FALSE == is_owner(caller)) {
           return ERR_NO_PERMISSION;
       }
 
@@ -312,7 +308,48 @@ int main(int argc, char *argv[]) {
       return SUCCESS;
   }
 
-  else if (strcmp(argv[1], ANN_RESULT)) {
+  // Vote yea or nay to ballot topic
+  else if (strcmp(argv[1], VOTE) == 0) {
+      if (argc != 3) {
+          return ERR_METHOD_ARG_NUM;
+      }
+
+      if (STARTED != state) {
+          return ERR_VOTE_NOT_STARTED_YET;
+      }
+
+      uint8_t vote;
+      if (strcmp(argv[2], YEA_STR)) {
+          vote = YEA;
+      }
+      else if (strcmp(argv[2], NAY_STR)) {
+          vote = NAY;
+      }
+      else {
+          return ERR_INVALID_VOTE_VALUE;
+      }
+
+      addr_t caller;
+      pvm_caller(caller.addr);
+
+      if (FALSE == has_vote_right(caller)) {
+          return ERR_NO_PERMISSION;
+      }
+
+      voter_t voter;
+      pvm_load(caller.addr, ADDRESS_SIZE, voter.state, sizeof(voter.state), NULL);
+
+      if (NONE != voter.state[1]) {
+          return ERR_ALREADY_VOTED;
+      }
+
+      voter.state[1] = vote;
+      pvm_save(caller.addr, ADDRESS_SIZE, voter.state, sizeof(voter.state));
+
+      return SUCCESS;
+  }
+
+  else if (strcmp(argv[1], ANN_RESULT) == 0) {
       if (argc != 2) {
           return ERR_METHOD_ARG_NUM;
       }
@@ -329,4 +366,57 @@ int main(int argc, char *argv[]) {
   }
 
   return SUCCESS;
+}
+
+int is_empty(uint8_t* val, uint64_t max_size) {
+    if (max_size > 1024) {
+        return ERR_EXCEE_MAX_LEN;
+    }
+
+    for (int i = 0; i < max_size; ++i) {
+        if (val[i] != 0) {
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
+int caller_mached(addr_t caller, addr_t expect) {
+  for (int i = 0; i < ADDRESS_SIZE; ++i) {
+    if (caller.addr[i] != expect.addr[i]) {
+      return FALSE;
+    }
+  }
+
+  return TRUE;
+}
+
+int is_owner(addr_t owner) {
+    addr_t caller;
+    pvm_caller(caller.addr);
+
+    return caller_mached(caller, owner);
+}
+
+int has_vote_right(addr_t caller) {
+    voter_t voter;
+    pvm_load(caller.addr, ADDRESS_SIZE, voter.state, sizeof(voter.state), NULL);
+
+    if (HAS_VOTER_RIGHT == voter.state[0]) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+int already_voted(addr_t caller) {
+    voter_t voter;
+    pvm_load(caller.addr, ADDRESS_SIZE, voter.state, sizeof(voter.state), NULL);
+
+    if (NONE != voter.state[1]) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,150 @@
 #[cfg(test)]
 mod tests {
-    use ethereum_types::U256;
+    use cita_vm::{Context, FakeVM, InterpreterResult, InterpreterType, Transaction};
+    use ethereum_types::{Address, U256};
 
-    use std::fs;
+    use std::{fs, ops::Deref, rc::Rc};
 
     const BALLOT_BINARY_PATH: &str = "./build/ballot";
 
-    fn read_le_u64(input: &mut &[u8]) -> u64 {
-        use std::convert::TryInto;
+    #[derive(Debug, Clone)]
+    struct User {
+        addr: Address,
+        nonce: Rc<usize>,
+    }
 
-        let (int_bytes, rest) = input.split_at(std::mem::size_of::<u64>());
-        *input = rest;
-        u64::from_le_bytes(int_bytes.try_into().unwrap())
+    impl Deref for User {
+        type Target = Address;
+
+        fn deref(&self) -> &Address {
+            &self.addr
+        }
+    }
+
+    impl User {
+        pub fn new(addr: Address) -> Self {
+            User {
+                addr,
+                nonce: Rc::new(1),
+            }
+        }
+
+        pub fn nonce(&mut self) -> U256 {
+            let nonce = *self.nonce.as_ref();
+            *Rc::get_mut(&mut self.nonce).expect("nonce") = nonce + 1;
+
+            U256::from(nonce)
+        }
+    }
+
+    struct Ballot {
+        vm: FakeVM,
+        addr: Address,
+    }
+
+    impl Ballot {
+        pub fn deploy(vm: FakeVM, owner: &mut User) -> Self {
+            let tx = Transaction {
+                from: **owner,
+                to: None,
+                value: U256::from(0),
+                nonce: owner.nonce(),
+                gas_limit: 1_000_000,
+                gas_price: U256::from(1),
+                input: fs::read(BALLOT_BINARY_PATH).expect("read contract"),
+                itype: InterpreterType::RISCV,
+            };
+
+            let addr = match vm.executor.exec(Context::default(), tx).expect("deploy") {
+                InterpreterResult::Create(_, _, _, addr) => addr,
+                _ => unreachable!(),
+            };
+
+            let ballot = Ballot { vm, addr };
+            ballot.set_owner(owner);
+
+            ballot
+        }
+
+        pub fn get_owner(&self, caller: &mut User) -> Address {
+            let tx = self.create_tx(caller, vec!["get_owner".into()]);
+            let val = self.exec_unwrap(tx, "get_owner");
+
+            Address::from_slice(&val)
+        }
+
+        fn set_owner(&self, caller: &mut User) {
+            let tx = self.create_tx(caller, vec!["set_owner".into()]);
+            self.exec_unwrap(tx, "set_owner");
+        }
+
+        pub fn get_topic(&self, caller: &mut User) -> String {
+            let tx = self.create_tx(caller, vec!["get_topic".into()]);
+            let val = self.exec_unwrap(tx, "get_topic");
+
+            String::from_utf8(val).expect("parse topic")
+        }
+
+        pub fn set_topic(&self, owner: &mut User, topic: &str) {
+            let tx = self.create_tx(owner, vec!["set_topic".into(), topic.into()]);
+            self.exec_unwrap(tx, "set_topic");
+        }
+
+        pub fn is_voter(&self, caller: &mut User) -> bool {
+            let tx = self.create_tx(caller, vec!["is_voter".into()]);
+            let val = self.exec_unwrap(tx, "is_voter");
+
+            if val.get(0) == Some(&1) {
+                true
+            } else {
+                false
+            }
+        }
+
+        pub fn authorize_vote_right(&self, owner: &mut User, user: &User) {
+            let user_addr = hex::encode(user.to_vec()).as_bytes().to_vec();
+
+            let tx = self.create_tx(owner, vec!["authorize_vote_right".into(), user_addr]);
+
+            self.exec_unwrap(tx, "authorize_vote_right");
+        }
+
+        fn create_tx(&self, caller: &mut User, params: Vec<Vec<u8>>) -> Transaction {
+            Transaction {
+                from: **caller,
+                to: Some(self.addr),
+                value: U256::from(0),
+                nonce: caller.nonce(),
+                gas_limit: 1_000_000,
+                gas_price: U256::from(1),
+                input: cita_vm::riscv::combine_parameters(params),
+                itype: cita_vm::InterpreterType::RISCV,
+            }
+        }
+
+        fn exec_unwrap(&self, tx: Transaction, name: &'static str) -> Vec<u8> {
+            match self.vm.executor.exec(Context::default(), tx).expect(name) {
+                InterpreterResult::Normal(val, ..) => val,
+                _ => panic!(name),
+            }
+        }
     }
 
     #[test]
     fn test_ballot() {
         let vm = cita_vm::FakeVM::new();
+        let mut owner = User::new(vm.account1);
+        let mut voter = User::new(vm.account2);
 
-        // Deploy contract
-        let tx = cita_vm::Transaction {
-            from: vm.account1,
-            to: None,
-            value: U256::from(0),
-            nonce: U256::from(1),
-            gas_limit: 1_000_000,
-            gas_price: U256::from(1),
-            input: fs::read(BALLOT_BINARY_PATH).unwrap(),
-            itype: cita_vm::InterpreterType::RISCV,
-        };
+        let ballot = Ballot::deploy(vm, &mut owner);
+        assert_eq!(ballot.get_owner(&mut voter), *owner, "should have owner");
 
-        let r = vm
-            .executor
-            .exec(cita_vm::Context::default(), tx)
-            .expect("deploy ballot contract");
+        ballot.set_topic(&mut owner, "Moon ?");
+        assert_eq!(&ballot.get_topic(&mut voter), "Moon ?", "should set topic");
 
-        let contract_address = match r {
-            cita_vm::InterpreterResult::Create(_, _, _, a) => a,
-            _ => unreachable!(),
-        };
+        assert_eq!(ballot.is_voter(&mut voter), false, "no right");
 
-        println!("{}", contract_address);
-
-        let tx = cita_vm::Transaction {
-            from: vm.account1,
-            to: Some(contract_address),
-            value: U256::from(0),
-            nonce: U256::from(2),
-            gas_limit: 1_000_000,
-            gas_price: U256::from(1),
-            input: cita_vm::riscv::combine_parameters(vec!["owner".into()]),
-            itype: cita_vm::InterpreterType::RISCV,
-        };
-
-        let r = vm
-            .executor
-            .exec(cita_vm::Context::default(), tx)
-            .expect("test address");
-
-        let addr = match r {
-            cita_vm::InterpreterResult::Normal(val, ..) => val,
-            _ => unreachable!(),
-        };
-
-        println!("address {:?}", ethereum_types::Address::from_slice(&addr));
+        ballot.authorize_vote_right(&mut owner, &voter);
+        assert_eq!(ballot.is_voter(&mut voter), true, "should have right");
     }
 }
